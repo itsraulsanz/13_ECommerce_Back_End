8488e1d3469491f370995b1fb0af2e23f2642982
Raul Sanz <luar_znas@hotmail.com>
2021-05-20 14:10:55 +0100

    MODELS/index.js: Configurate the associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_tag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  foreignKey: 'product_tag',
+});
 
 module.exports = {
   Product,
