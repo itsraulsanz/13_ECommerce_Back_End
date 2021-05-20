e16c2fb2ebe83cd2be6aaddd567d41619dc22c76
Raul Sanz <luar_znas@hotmail.com>
2021-05-20 18:18:18 +0100

    MODELS/index.js: Fixed the associations

--- models/index.js
+++ models/index.js
@@ -5,19 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_name',
+});
 
 // Categories have many Products
 Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_tag',
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  //as: 'planned_trips'
 });
 
+
+
 // Tags belongToMany Products (through ProductTag)
-Tags.belongsToMany(Product, {
-  foreignKey: 'product_tag',
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  //as: 'planned_trips'
 });
 
 module.exports = {
